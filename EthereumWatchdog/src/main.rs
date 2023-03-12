700de831c2b982b146aa4db5804aa8eb9644a58a
D4r3-D3v1l <a.surya842000@gmail.com>
2023-03-12 18:48:54 +0530

    changed taking latest block to taking block from watch_blocks()

--- EthereumWatchdog/src/main.rs
+++ EthereumWatchdog/src/main.rs
@@ -10,47 +10,42 @@ use std::io::Write;
 async fn main() -> anyhow::Result<()> {
     // Connect to the Ethereum network using Infura
     let provider = Provider::<Http>::try_from("https://goerli.infura.io/v3/c60b0bb42f8a4c6481ecd229eddaca27")?;
-    let block_stream = provider.watch_blocks().await?;
+    let mut block_stream = provider.watch_blocks().await?;
 
-    // Loop through new block events
-    block_stream.for_each(|block| async {
+    while let Some(block) = block_stream.next().await {
 
-        // let block_number = block.number.unwrap().as_u64();
-        // println!("New block: {}", block_number);
+		let block_header = provider.get_block_with_txs(block).await?;
 
-        // // Get the list of transactions in the new block
-        // let txns = block.transactions.unwrap_or_default();
-		let latest_block = provider.get_block(BlockNumber::Latest).await.unwrap().unwrap();
+		if let Some(block_header) = block_header {
+			let txns = block_header.transactions;
+			for txn in txns {
 
-		let txns = latest_block.transactions.iter();
-        // Check each transaction to see if it meets the specified criteria
-        for txn in txns {
-            let txn_hash = txn.clone();
-            let txn_data = provider.get_transaction(txn_hash).await.unwrap().unwrap();
+				
+				let from = format_address(txn.from);
+				let to = format_address(txn.from);
+				let hash = format!("0x{}", hex::encode(txn.hash));
+				let value = txn.value.as_u128();
+				
+    
+				let txn_string = format!("Transaction Hash: {}, from: {}, to: {}, value: {}", hash, from, to, value);
 
-            let from = format_address(txn_data.from);
-            let to = match txn_data.to {
-                Some(to) => format_address(to),
-                None => "None".to_string(),
-            };
-            let value = txn_data.value.as_u128();
-			let txn_string = format!("Transaction Hash: {}, from: {}, to: {}, value: {}\n", txn_hash, from, to, value);
+			    let file_name = format!("block_{}.txt", block_header.number.unwrap().as_u64());
+			    let path = Path::new(&file_name);
 
-			let file_name = format!("block_{}.txt", latest_block.number.unwrap().as_u64());
-            let path = Path::new(&file_name);
+			    // Open the file in append mode
+			    let mut file = OpenOptions::new()
+			        .create(true)
+			        .append(true)
+			        .open(&path)
+			        .unwrap();
 
-            // Open the file in append mode
-            let mut file = OpenOptions::new()
-                .create(true)
-                .append(true)
-                .open(&path)
-                .unwrap();
+			    file.write_all(txn_string.as_bytes());
+				println!("{}",txn_string);
+			}
+			
+		}        
+    }
 
-            file.write_all(txn_string.as_bytes());
-            println!("{}",txn_string);
-        }
-        
-    }).await;
 
     Ok(())
 }
